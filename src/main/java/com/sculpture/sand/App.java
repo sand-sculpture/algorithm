05a2b5c26fa5558a199192a518dcef8682cc1c49
niezheng1 <niezheng1@jd.com>
2019-06-27 19:07:38 +0800

    init

---
+++ src/main/java/com/sculpture/sand/App.java
@@ -0,0 +1,13 @@
+package com.sculpture.sand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
