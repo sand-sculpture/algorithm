89d61508c3492dbc4e9d8569e5ff4ed10e22d6bb
gaohaitao <3001563977@qq.com>
2019-06-28 09:30:22 +0800

    ght:构建乘积数组

--- src/main/java/com/sculpture/sand/ght/BuildingAProductArray.java
+++ src/main/java/com/sculpture/sand/ght/BuildingAProductArray.java
@@ -2,6 +2,9 @@ package com.sculpture.sand.ght;
 
 /**
  * @description: 构建成绩数组
+ *
+ * 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。不能使用除法。
+ *
  * @author: haitao.gao
  * @create: 2019-06-27 20:58
  **/
