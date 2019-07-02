ee91aa56a71b98e49b35f91259b8c7c7b5698ee7
niezheng1 <niezheng1@jd.com>
2019-07-02 09:33:28 +0800

    暴力破解

--- src/main/java/com/sculpture/sand/zheng/nie/array/Duplicate.java
+++ src/main/java/com/sculpture/sand/zheng/nie/array/Duplicate.java
@@ -1,5 +1,9 @@
 package com.sculpture.sand.zheng.nie.array;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author: niezheng1
  * @Date: 2019/7/2 9:09
@@ -8,13 +12,31 @@ package com.sculpture.sand.zheng.nie.array;
  *      数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。
  *      请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
  *
- * 思路：hashMap计数
+ *
  *
  */
 public class Duplicate {
 
-
+    /**
+     * 思路：先暴力破解 hashSet计数
+     * @param numbers
+     * @param length
+     * @param duplication
+     * @return
+     */
     public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(null == numbers || length == 0){
+            return false;
+        }
+        Set set = new HashSet();
+        for (int i= 0;i<numbers.length;i++){
+            if(set.contains(numbers[i])){
+                duplication[0] = numbers[i];
+                return true;
+            }else {
+                set.add(numbers[i]);
+            }
+        }
         return false;
     }
 
