cb678b82dd82adb2202416cb740a2d4eb88aa265
niezheng1 <niezheng1@jd.com>
2019-07-03 09:19:58 +0800

    第四题

---
+++ src/main/java/com/sculpture/sand/zheng/nie/array/FindNumsAppearOnce.java
@@ -0,0 +1,19 @@
+package com.sculpture.sand.zheng.nie.array;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/7/3 9:18
+ *
+ * 题目：一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ *
+ *  num1,num2分别为长度为1的数组。传出参数
+    将num1[0],num2[0]设置为返回结果
+ *
+ */
+public class FindNumsAppearOnce {
+
+    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+
+    }
+
+}
