062e83845323378f7febaad94a4e23beb94e444c
niezheng1 <niezheng1@jd.com>
2019-07-03 18:24:06 +0800

    位运算符

--- src/main/java/com/sculpture/sand/zheng/nie/array/FindNumsAppearOnce.java
+++ src/main/java/com/sculpture/sand/zheng/nie/array/FindNumsAppearOnce.java
@@ -4,11 +4,13 @@ package com.sculpture.sand.zheng.nie.array;
  * @author: niezheng1
  * @Date: 2019/7/3 9:18
  *
+ * 异或：相同为0 相异为1
+ *
  * 题目：一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
  *
  *  num1,num2分别为长度为1的数组。传出参数
     将num1[0],num2[0]设置为返回结果
- *
+ * [1,2,4,5,6,7,5,1,2,6]
  */
 public class FindNumsAppearOnce {
 
