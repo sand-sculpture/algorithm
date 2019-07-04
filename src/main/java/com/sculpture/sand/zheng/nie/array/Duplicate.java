2d376ab5b1c4d8b8ff3ccaf2cefb14b35ee9c57b
niezheng1 <niezheng1@jd.com>
2019-07-04 15:30:38 +0800

    思路

--- src/main/java/com/sculpture/sand/zheng/nie/array/Duplicate.java
+++ src/main/java/com/sculpture/sand/zheng/nie/array/Duplicate.java
@@ -1,6 +1,5 @@
 package com.sculpture.sand.zheng.nie.array;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,7 +11,10 @@ import java.util.Set;
  *      数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。
  *      请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
  *
- *
+ * 思路：
+ *      1.循环数组，依次用数组值求余数组长度，因为数值的值 是在0 - n-1之间 那么 求余之后的数要是相等 数组中的数就必定相等
+ *      2.依次对数组中的数字进行标记+length
+ *      3.如果循环到相等数第二次的时候，次数该数组已求余数为下标的值 必定大于length 也必定是第一个相同的数
  *
  */
 public class Duplicate {
