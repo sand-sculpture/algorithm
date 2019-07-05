9c6208e8266ebf81f22130ee8fbdc8908e24072c
Binaryzn <18162327446@163.com>
2019-07-05 17:23:31 +0800

    第五题 查询有数数组中数出现的次数

---
+++ src/main/java/com/sculpture/sand/zheng/nie/array/GetNumberOfK.java
@@ -0,0 +1,29 @@
+package com.sculpture.sand.zheng.nie.array;
+
+
+/**
+ * 题目：统计一个数字在排序数组中出现的次数。
+ * 思路：二分查找， 高位下标减去低位下表就是出现的次数
+ *
+ *
+ */
+public class GetNumberOfK {
+
+
+    public int GetNumberOfK(int [] array , int k) {
+        if(array == null || array.length == 0){
+            return 0;
+        }
+        return 0;
+    }
+
+
+    private int getHighIndex(int [] array,int k ){
+        return 0;
+    }
+
+    private int getLowIndex(int[] array,int k){
+        return 0;
+    }
+
+}
