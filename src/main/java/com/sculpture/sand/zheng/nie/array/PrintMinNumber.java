eb5a919fbdc812acd4f9b156dcc794f6cb21055f
niezheng1 <niezheng1@jd.com>
2019-07-09 09:14:44 +0800

    第六题

---
+++ src/main/java/com/sculpture/sand/zheng/nie/array/PrintMinNumber.java
@@ -0,0 +1,17 @@
+package com.sculpture.sand.zheng.nie.array;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/7/9 9:09
+ *输入一个正整数数组，把数组里所有数字拼接起来排成一个数，
+ * 打印能拼接出的所有数字中最小的一个。例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
+ *
+ */
+public class PrintMinNumber {
+
+
+    public void PrintMinNumber( ) {
+        int [] numbers = new int[]{2,3,4,5,555};
+    }
+
+}
