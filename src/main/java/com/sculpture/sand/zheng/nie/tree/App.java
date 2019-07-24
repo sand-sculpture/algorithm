524d4ae3f3ad11a34f6ac423addaaa20484101a6
niezheng1 <niezheng1@jd.com>
2019-07-24 10:49:44 +0800

    判断树的路径之和

---
+++ src/main/java/com/sculpture/sand/zheng/nie/tree/App.java
@@ -0,0 +1,16 @@
+package com.sculpture.sand.zheng.nie.tree;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/7/24 10:01
+ */
+public class App {
+
+    public static void main(String[] args) {
+        String s = "0123456";
+        System.out.println("l is : "+s);
+        String substring = s.substring(0, 4);
+        System.out.println(substring);
+    }
+
+}
