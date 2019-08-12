f46ded89b2d2988b4c4ad650106741e6c35d127d
niezheng1 <niezheng1@jd.com>
2019-08-12 15:18:44 +0800

    递归版本 链表合并

--- src/main/java/com/sculpture/sand/zheng/nie/ai/list/Merge.java
+++ src/main/java/com/sculpture/sand/zheng/nie/ai/list/Merge.java
@@ -9,6 +9,54 @@ package com.sculpture.sand.zheng.nie.ai.list;
  */
 public class Merge {
     public ListNode Merge(ListNode list1,ListNode list2) {
-        return null;
+        if(list1 == null){
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+        ListNode head = new ListNode(-1);
+        ListNode root = head;
+        while (list1!= null && list2 != null){
+            if(list1.val>=list2.val){
+                root.next = list2;
+                list2 = list2.next;
+                root = root.next;
+            }else {
+                root.next = list1;
+                list1 = list1.next;
+                root = root.next;
+            }
+        }
+        //
+        if(list1 == null){
+            root.next = list2;
+        }
+        if(list2 == null){
+            root.next = list1;
+        }
+        return head.next;
+    }
+
+    /**
+     * 递归版本
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public ListNode Merg1(ListNode list1,ListNode list2) {
+        if(list1 == null){
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+        if(list1.val <= list2.val){
+            list1.next = Merg1(list1.next,list2);
+            return list1;
+        }else {
+            list2.next = Merg1(list1,list2.next);
+            return list2;
+        }
     }
 }
