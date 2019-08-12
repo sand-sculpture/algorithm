6d27f9acd44957b92dd0e92b343a69d38c263f1d
niezheng1 <niezheng1@jd.com>
2019-08-12 15:21:42 +0800

    复杂链表的复制

---
+++ src/main/java/com/sculpture/sand/zheng/nie/ai/list/RandomListNode.java
@@ -0,0 +1,15 @@
+package com.sculpture.sand.zheng.nie.ai.list;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/8/12 15:19
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
