8710e4b6fd447bb2427e0f32ef63ba957140d3cb
niezheng1 <niezheng1@jd.com>
2019-08-12 14:57:32 +0800

    题目

---
+++ src/main/java/com/sculpture/sand/zheng/nie/ai/list/Merge.java
@@ -0,0 +1,14 @@
+package com.sculpture.sand.zheng.nie.ai.list;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/8/12 14:54
+ *
+ * 题目：输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+ *
+ */
+public class Merge {
+    public ListNode Merge(ListNode list1,ListNode list2) {
+        return null;
+    }
+}
