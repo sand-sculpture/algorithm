e62c8272cabc06bdf61551b9d3761d82470b7d72
niezheng1 <niezheng1@jd.com>
2019-08-19 14:48:18 +0800

    数组中的两数之和

---
+++ src/main/java/com/sculpture/sand/zheng/nie/leetcode/ListNode.java
@@ -0,0 +1,16 @@
+package com.sculpture.sand.zheng.nie.leetcode;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/8/19 14:46
+ */
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
