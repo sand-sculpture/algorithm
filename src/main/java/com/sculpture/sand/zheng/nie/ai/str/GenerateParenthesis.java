697802b0cb786a0f41113adc27ff0d0cf4bf8a68
niezheng1 <niezheng1@jd.com>
2019-08-29 18:50:15 +0800

    本地test

---
+++ src/main/java/com/sculpture/sand/zheng/nie/ai/str/GenerateParenthesis.java
@@ -0,0 +1,20 @@
+package com.sculpture.sand.zheng.nie.ai.str;
+
+import java.util.List;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/8/21 15:43
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ *
+ *  n = 1 : ()
+ *  n = 2 : (()) ()()
+ *  n = 3
+ */
+public class GenerateParenthesis {
+
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+
+}
