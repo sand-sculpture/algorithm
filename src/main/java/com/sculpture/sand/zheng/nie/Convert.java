1fa5dcf7ac8c46560bf1e2d57393779c2ae84c83
niezheng1 <niezheng1@jd.com>
2019-10-09 19:46:35 +0800

    Z字型打印

--- src/main/java/com/sculpture/sand/zheng/nie/Convert.java
+++ src/main/java/com/sculpture/sand/zheng/nie/Convert.java
@@ -2,7 +2,10 @@ package com.sculpture.sand.zheng.nie;
 
 import org.junit.Test;
 
-/**
+import java.util.ArrayList;
+import java.util.List;
+
+/** 题目：将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
  * @author: niezheng1
  * @Date: 2019/9/5 20:36
  */
@@ -11,17 +14,29 @@ public class Convert {
     // LEETCODEISHIRING
 
     public String convert(String s, int numRows) {
-        char[] chars = s.toCharArray();
-        StringBuffer buffer = new StringBuffer();
-        for (int i = 0;i<numRows;i++){
-            buffer.append(chars[i]);
-            int j =i+numRows+1;
-            while (j<chars.length){
-                buffer.append(chars[j]);
-                j = j+numRows+1;
+       if(numRows <2){
+           return s;
+       }
+        List<StringBuffer> row = new ArrayList<>();
+       for (int i=0;i<numRows;i++){
+           //记录行层数
+           StringBuffer buffer = new StringBuffer();
+           row.add(buffer);
+       }
+       int i = 0;
+       int flag = -1;
+       for (char item : s.toCharArray()){
+            row.get(i).append(item);
+            if(i ==0 || i == numRows-1){
+                flag = -flag;
             }
-        }
-       return buffer.toString();
+            i+= flag;
+       }
+       StringBuilder stringBuilder = new StringBuilder();
+       for (StringBuffer stringBuffer : row){
+            stringBuilder.append(stringBuffer);
+       }
+       return stringBuilder.toString();
     }
 
     @Test
