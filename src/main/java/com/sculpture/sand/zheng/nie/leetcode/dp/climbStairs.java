c92a94f0a81a90fdc77eb2fa094d7e7de367cf0a
niezheng1 <niezheng1@jd.com>
2019-11-20 20:42:50 +0800

    dp

---
+++ src/main/java/com/sculpture/sand/zheng/nie/leetcode/dp/climbStairs.java
@@ -0,0 +1,34 @@
+package com.sculpture.sand.zheng.nie.leetcode.dp;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/11/20 20:34
+ * <p>
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * <p>
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * <p>
+ * 注意：给定 n 是一个正整数。
+ */
+public class climbStairs {
+
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+
+}
