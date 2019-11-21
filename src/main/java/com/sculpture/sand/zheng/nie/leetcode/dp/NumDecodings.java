acc9fabc8860e4705187154ac834369e96851e23
niezheng1 <niezheng1@jd.com>
2019-11-21 10:09:59 +0800

    dp

---
+++ src/main/java/com/sculpture/sand/zheng/nie/leetcode/dp/NumDecodings.java
@@ -0,0 +1,19 @@
+package com.sculpture.sand.zheng.nie.leetcode.dp;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/11/21 10:00
+ *
+ *
+ * https://leetcode-cn.com/problems/decode-ways/
+ *
+ *
+ */
+public class NumDecodings {
+
+
+    public int numDecodings(String s) {
+        return 0;
+    }
+
+}
