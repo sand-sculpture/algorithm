7975b7545a3142d9ac07e895415519aa74d2aaf8
niezheng1 <niezheng1@jd.com>
2019-11-21 15:41:22 +0800

    笔记

---
+++ src/test/java/com/sculpture/sand/zheng/TEstTest.java
@@ -0,0 +1,16 @@
+package com.sculpture.sand.zheng;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/11/21 10:51
+ */
+public class TEstTest {
+
+    @Test
+    public void main() {
+    }
+}
