31f87e5aeb17139c33b9d410f041f73b14edb8a3
Binaryzn <18162327446@163.com>
2019-11-25 22:37:59 +0800

    三角形之和

--- src/main/java/com/sculpture/sand/zheng/nie/leetcode/dp/MinimumTotal.java
+++ src/main/java/com/sculpture/sand/zheng/nie/leetcode/dp/MinimumTotal.java
@@ -23,21 +23,42 @@ import java.util.List;
  */
 public class MinimumTotal {
 
+    public int minimumTotal1(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0){
+            return 0;
+        }
+        //size+1的好处时 在计算最底层的时候 不用初始化
+      int[][] dp = new int[triangle.size()+1][triangle.size()+1];
+
+        for (int i =triangle.size()-1;i>=0;i--){
+            List<Integer> rowList = triangle.get(i);
+            for (int j = 0;j<triangle.get(i).size();j++){
+                dp[i][j] = Math.min(dp[i+1][j],dp[i+1][j+1])+rowList.get(j);
+            }
+        }
+        return dp[0][0];
+    }
+
+
     public int minimumTotal(List<List<Integer>> triangle) {
         if (triangle == null || triangle.size() == 0){
             return 0;
         }
-        // 只需要记录每一层的最小值即可
+        //不用初始化最后一行
         int[] dp = new int[triangle.size()+1];
-
-        for (int i = triangle.size() - 1; i >= 0; i--) {
-            List<Integer> curTr = triangle.get(i);
-            for (int j = 0; j < curTr.size(); j++) {
-                //这里的dp[j] 使用的时候默认是上一层的，赋值之后变成当前层
-                dp[j] = Math.min(dp[j],dp[j+1]) + curTr.get(j);
+        for (int i = triangle.size()-1;i>=0;i--){
+            List<Integer> rowList = triangle.get(i);
+            for (int j = 0;j<rowList.size();j++){
+                //min方法中的dp[j] 每次都是上一行的 dp初始化时size+1进行的初始化第一行
+                //每次执行一层循环之后 实际上相当于计算了本行所有的dp[j] 的最小路径
+                dp[j] = Math.min(dp[j],dp[j+1]) +rowList.get(j);
             }
         }
         return dp[0];
     }
+
+
+
+
 
 }
